261359dddaab2e86a395e78c81c2a957bd1efac8
vruiz-ru <161227190+vruiz-ru@users.noreply.github.com>
2024-02-25 15:55:36 +0100

    Add files via upload

---
+++ ft_isalnum.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vruiz-ru <vruiz-ru@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/11 20:22:51 by vruiz-ru          #+#    #+#             */
+/*   Updated: 2024/01/11 20:36:17 by vruiz-ru         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <ctype.h>
+
+int	ft_isalnum(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || ( c >= '0' && c <= '9'))
+		return (1);
+	else
+		return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	(void)argc;
+
+	argv++;
+	printf("char    :%c\n", **argv);
+	printf("ft      :%d\n", ft_isalnum(**argv));
+	printf("original:%d\n", isalnum(**argv));
+	return (0);
+}
