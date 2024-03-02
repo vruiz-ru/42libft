5ae584fc8a32d8cb543746276dd3b5498ead8643
vruiz-ru <161227190+vruiz-ru@users.noreply.github.com>
2024-03-02 20:02:08 +0100

    Add files via upload

---
+++ ft_substr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vruiz-ru <vruiz-ru@student.42madrid>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/02 19:13:16 by vruiz-ru          #+#    #+#             */
+/*   Updated: 2024/03/02 19:56:54 by vruiz-ru         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "libft.h"
+
+char *ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t s_len;
+	char *s;
+	
+	if (start >= s_len)
+		return (ft_strup(""))
+	if 		
+
+
+
+}
